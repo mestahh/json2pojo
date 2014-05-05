75ee7c588878eb2d8f68a409b9472ae0e29f536f
mestahh <balazs.mester@gmail.com>
2014-05-05 07:13:21 +0200

    Create nested classes.

--- lib/pojo_creator.rb
+++ lib/pojo_creator.rb
@@ -8,24 +8,34 @@ class PojoCreator
 
   def create_pojo(json_string)
     json = JSON.parse(json_string)
-    pojo = "public class #{@class_name} {#{get_properties(json)}\n\t\t\t}"
+    pojo = "public class #{@class_name} {#{get_properties(json)}}" + @additional_classes
   end
+
   def get_properties(json)
-  
     prop_string = ""
+    @additional_classes = ""
     json.keys.each do |property|
-      prop_string += "\n\t\t\t@JsonProperty(\"#{property}\")\n\t\t\tprivate #{get_type(json[property])} #{property};"
+      if (json[property].class.to_s == "Hash")
+        @additional_classes += "public class #{get_class_name(property)} {" + get_properties(json[property]) + "}"
+        prop_string += "\t@JsonProperty(\"#{property}\")\tprivate Text #{property};"
+      else
+        prop_string += "\t@JsonProperty(\"#{property}\")\tprivate #{get_type(json[property])} #{property};"
+      end
     end
     prop_string
+
+  end
+
+  def get_class_name(property) 
+    property.capitalize
   end
 
   def get_type(value)
-  	puts value.class
-  	if value.class.to_s == "Fixnum"
-  		return "Integer"
-  	else 
-  		return "String"
-  	end
+    if value.class.to_s == "Fixnum"
+      return "Integer"
+    else
+      return "String"
+    end
   end
 
 end
--- spec/pojo_creator_spec.rb
+++ spec/pojo_creator_spec.rb
@@ -1,18 +1,34 @@
 require_relative '../lib/pojo_creator'
 
 describe PojoCreator do
-	it "creates an POJO with an int" do
-		PojoCreator.new("ClassName").create_pojo('{"time" : 3}').should eq('public class ClassName {
-			@JsonProperty("time")
-			private Integer time;
-			}')
-	end
+  it "creates an POJO with an int" do
+    PojoCreator.new("ClassName").create_pojo('{"time" : 3}').should eq(
+      'public class ClassName {'\
+      '	@JsonProperty("time")'\
+      '	private Integer time;'\
+    '}')
+  end
 
-	it "creates an POJO with a String" do
-		PojoCreator.new("OtherClass").create_pojo('{"value" : "something"}').should eq('public class OtherClass {
-			@JsonProperty("value")
-			private String value;
-			}')
-	end
+  it "creates an POJO with a String" do
+    PojoCreator.new("ClassName").create_pojo('{"text" : "something"}').should eq(
+      'public class ClassName {'\
+      '	@JsonProperty("text")'\
+      '	private String text;'\
+    '}')
+  end
+
+  it "creates an POJO with an Object inside with one string field" do
+    PojoCreator.new("ClassName").create_pojo('{"text" : { "text" : "something"}}').should eq(
+      "public class ClassName {"\
+      "\t@JsonProperty(\"text\")"\
+      "\tprivate Text text;"\
+    "}"\
+    "public class Text {"\
+    "\t@JsonProperty(\"text\")"\
+    "\tprivate String text;"\
+    "}")
+  end
+
+
 
 end
