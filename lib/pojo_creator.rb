852fb5ac3362aa4e0598b0cea24120c0710c8d15
mestahh <balazs.mester@gmail.com>
2014-05-05 15:29:18 +0200

    Refactor.

--- lib/pojo_creator.rb
+++ lib/pojo_creator.rb
@@ -2,7 +2,7 @@ require "json"
 
 class PojoCreator
 
-  VERSION = '0.2.0'
+  VERSION = '0.3.0'
 
   def initialize(class_name)
     @class_name = get_class_name(class_name)
@@ -18,16 +18,18 @@ class PojoCreator
 
   def get_properties(json)
     prop_string = ""
-
+    
     json.keys.each do |property|
+      prop_prefix = "\t@JsonProperty(\"#{property}\")\tprivate "
+
       if (json[property].class.to_s == "Hash")
         @additional_classes << "class #{get_class_name(property)} {" + get_properties(json[property]) + "}"
-        prop_string += "\t@JsonProperty(\"#{property}\")\tprivate #{get_class_name(property)} #{get_field_name(property)};"
+        prop_string += "#{prop_prefix}#{get_class_name(property)} #{get_field_name(property)};"
       elsif(json[property].class.to_s == "Array")
         @additional_classes << "class #{get_class_name(property)} {" + get_properties(json[property][0]) + "}"
-        prop_string += "\t@JsonProperty(\"#{property}\")\tprivate List<#{get_class_name(property)}> #{get_field_name(property)};"
+        prop_string += "#{prop_prefix}List<#{get_class_name(property)}> #{get_field_name(property)};"
       else
-        prop_string += "\t@JsonProperty(\"#{property}\")\tprivate #{get_type(json[property])} #{get_field_name(property)};"
+        prop_string += "#{prop_prefix}#{get_type(json[property])} #{get_field_name(property)};"
       end
     end
     prop_string
@@ -56,7 +58,7 @@ class PojoCreator
       Date.parse(value)
       return "Date"
     rescue
-      
+
     end
 
     if value.class.to_s == "Fixnum"
